c76ed3c4e127c4976819f269a784593eccc20d84
plago-bergonpazos <plago-bergonpazos@nautilus21.danielcastelao.org>
2016-09-27 10:09:01 +0200

    Terminado

--- src/arquivosdir/Metodos.java
+++ src/arquivosdir/Metodos.java
@@ -12,16 +12,18 @@ import java.io.File;
  * @author oracle
  */
 public class Metodos {
+    
     String rutaArquivosdir = "/home/oracle/NetBeansProjects/arquivos/arquivosdir/";
     
     File arquivosdir = new File(rutaArquivosdir);
-
-    public boolean deleteDirectory(File path){
+    
+    //Recorre un 'array' tipo 'File' y borra los archivos
+    public boolean borrar(File path){
        if(arquivosdir.exists()){
            File[] files = arquivosdir.listFiles();
            for (int i = 0; i <files.length; i++){
                if(files[i].isDirectory()){
-                   deleteDirectory(files[i]);
+                   borrar(files[i]);
                }else{
                    
                files[i].delete();
